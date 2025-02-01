5fb62d55f7221a9e88fb06f68929f6493310ed9c
myBadGames <moses.yusufov@gmail.com>
2025-01-31 19:53:17 -0500

    have to change movement, made camera rotation smooth,

--- GreenSoldierAnimController/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ GreenSoldierAnimController/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -5,83 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
-    public Transform soldierTransform;
-    [SerializeField] private Vector3 move;
-    [SerializeField] private float speed = 1.0f;
+    private Transform soldierModel;
+    private Transform focalPoint;
+    [SerializeField] private float verticalInput;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float horizontalMouse;
+    [SerializeField] private Vector3 forwardDirection;
+    [SerializeField] private Vector3 strafeDirection;
+    [SerializeField] private float movingSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float rotationSlerpSpeed;
     public bool aiming;
     public bool walking;
     public bool running;
 
-    [SerializeField] private Quaternion targetRotation;
-    private Quaternion fwRotation = Quaternion.Euler(0, 0, 0);
-    private Quaternion bkRotation = Quaternion.Euler(0, 180, 0);
-    private Quaternion lRotation = Quaternion.Euler(0, -90, 0);
-    private Quaternion rRotation = Quaternion.Euler(0, 90, 0);   
-    private Quaternion fwLRotation = Quaternion.Euler(0, -45, 0);
-    private Quaternion fwRRotation = Quaternion.Euler(0, 45, 0);
-    private Quaternion bkLRotation = Quaternion.Euler(0, -135, 0);
-    private Quaternion bkRRotation = Quaternion.Euler(0, 135, 0);
-    [SerializeField] private float rotationSpeedSoldier = 2.5f;
-
-    [SerializeField] private Quaternion lookDirection;
-    void Start()
+    public float target;
+    public float focalY;
+    private float v = 0;
+    private float smoothTime = 0.15f;
+    private void Start()
     {
         controller = GetComponent<CharacterController>();
-        soldierTransform = transform.GetChild(0);
+        soldierModel = transform.GetChild(0);
+        focalPoint = transform.GetChild(1);
     }
 
-    void Update()
+    private void Update()
     {
-        PlayerInput();
-        Movement();
-        Conditions();
-        SoldierRotation();
+        RotateCamera();
     }
 
-    private void PlayerInput()
+    private void RotateCamera()
     {
-        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        horizontalMouse = Input.GetAxisRaw("Mouse X");
 
-        if (Input.GetButton("Fire2"))
-        {
-            aiming = true;
-        }
-        if (Input.GetButtonUp("Fire2"))
-        {
-            aiming = false;
-        }
-    }
+        target += horizontalMouse * rotationSpeed * Time.deltaTime;
+        focalY = Mathf.SmoothDampAngle(focalY, target,ref v, smoothTime);
 
-    private void Movement()
-    {
-        if (move != Vector3.zero)
-        {
-            if (!aiming)
-            {
-                controller.Move(move * Time.deltaTime * speed);
-            }
-        }
-    }
 
-    private void Conditions()
-    {
-        if (move != Vector3.zero)
-        {
-            walking = true;
-        }
-        else
-        {
-            walking = false;
-        }
-    }
-
-    private void SoldierRotation()
-    {
-        if (move != Vector3.zero)
-        {
-            lookDirection = Quaternion.LookRotation(move);
-
-            soldierTransform.localRotation = Quaternion.Slerp(soldierTransform.localRotation, lookDirection, Time.deltaTime * rotationSpeedSoldier);
-        }
+        focalPoint.localRotation = Quaternion.Euler(0, focalY, 0);
     }
 }
