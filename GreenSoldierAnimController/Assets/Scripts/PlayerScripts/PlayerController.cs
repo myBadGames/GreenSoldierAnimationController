8adc308fe99d384adadc28644684061d9b512ed0
myBadGames <moses.yusufov@gmail.com>
2025-01-31 19:59:05 -0500

    clean up

--- GreenSoldierAnimController/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ GreenSoldierAnimController/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -6,23 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
     private Transform soldierModel;
-    private Transform focalPoint;
     [SerializeField] private float verticalInput;
     [SerializeField] private float horizontalInput;
-    [SerializeField] private float horizontalMouse;
     [SerializeField] private Vector3 forwardDirection;
     [SerializeField] private Vector3 strafeDirection;
     [SerializeField] private float movingSpeed;
-    [SerializeField] private float rotationSpeed;
-    [SerializeField] private float rotationSlerpSpeed;
+
     public bool aiming;
     public bool walking;
     public bool running;
 
-    public float target;
-    public float focalY;
+    [SerializeField] private float horizontalMouse;
+    
+    private Transform focalPoint;
+    [SerializeField] private float rotationSpeed = 15.0f;
+    [SerializeField] private float targetY;
+    [SerializeField] private float focalY;
     private float v = 0;
-    private float smoothTime = 0.15f;
+    [SerializeField] private float smoothTime = 0.15f;
+
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -38,11 +40,8 @@ public class PlayerController : MonoBehaviour
     private void RotateCamera()
     {
         horizontalMouse = Input.GetAxisRaw("Mouse X");
-
-        target += horizontalMouse * rotationSpeed * Time.deltaTime;
-        focalY = Mathf.SmoothDampAngle(focalY, target,ref v, smoothTime);
-
-
+        targetY += horizontalMouse * rotationSpeed * Time.deltaTime;
+        focalY = Mathf.SmoothDampAngle(focalY, targetY, ref v, smoothTime);
         focalPoint.localRotation = Quaternion.Euler(0, focalY, 0);
     }
 }
